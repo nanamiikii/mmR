89fc928ddafec58a7e4cd2c4d43a7b252c89c4bf
Libby Brill <libby@brillfamily.net>
2026-06-05 00:03:32 -0700

    leaflet fixes, data wrangling function, test cases

--- R/plot-leaflet.R
+++ R/plot-leaflet.R
@@ -4,10 +4,8 @@
 #' measles_incidence_rate_per_1000000_total_population)
 #' @param low The color for the low end of the scale, as a string
 #' @param high The color for the high end of the scale, as a string
-#' @param format How the parameter should be formatted ("percent", "dollar",
-#' "comma")
-#' @param normalize Whether or not the variable should be normalized, should be
-#' TRUE for variables not on a 0-1 scale. Default is FALSE
+#' @param format Whether or not to format the variable (i.e. percents, dollar
+#' amounts, commas). Default is TRUE
 #'
 #' @import dplyr
 #' @import leaflet
@@ -20,33 +18,22 @@
 #' @export
 #'
 #' @examples
-#' plot_leaflet(var = mcv2, low = "#ecf0f1", high = "#e74c3c",
-#' format = "percent")
-#' plot_leaflet(var = GDP_per_capita, low = "#ecf0f1", high = "#1ccdaa",
-#' format = "dollar", normalize = TRUE)
+#' plot_leaflet(var = mcv2, low = "#ecf0f1", high = "#e74c3c")
+#' plot_leaflet(var = GDP_per_capita, low = "#ecf0f1", high = "#1ccdaa")
 #' plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
-#' low = "#ecf0f1", high = "#f4a324", format = "comma", normalize = TRUE)
-plot_leaflet <- function(var, low, high, format, normalize = FALSE) {
+#' low = "#ecf0f1", high = "#f4a324", format = FALSE)
+plot_leaflet <- function(var, low, high, format = TRUE) {
   var_name <- as_name(ensym(var))
   validate_var(var_name)
 
-  stopifnot(is.character(low),
-            is.character(high),
-            is.character(format),
-            is.logical(normalize))
+  validate_color(low)
+  validate_color(high)
 
-  if (!format %in% c("percent", "dollar", "comma")) {
-    stop('Please choose one of: "percent", "dollar", "comma"')
+  if (!is.logical(format)) {
+    stop("format should be logical")
   }
 
-  metadata_2024 <- load_data() |>
-    filter(year == 2024) |>
-    mutate(label_col = switch(format,
-                              "percent" = percent({{var}}),
-                              "dollar"  = dollar({{var}}),
-                              "comma"   = comma({{var}})),
-           scale_col = if (normalize) {rescale({{var}}, to = c(0, 1))}
-                       else {{var}})
+  metadata_2024 <- formatted_data(var_name, format)
 
   map_data <- ne_countries(scale = "medium", returnclass = "sf") |>
     left_join(metadata_2024, join_by(iso_a3_eh == iso3))
@@ -55,10 +42,10 @@ plot_leaflet <- function(var, low, high, format, normalize = FALSE) {
                       domain = c(0, 1),
                         na.color = "darkgray")
 
-  # parameter = switch(var_name,
-  #                    "mcv2" = "MCV2",
-  #                    "GDP_per_capita" = "GDP per capita",
-  #                    "measles_incidence_rate_per_1000000_total_population" = "Measles")
+  var_label = switch(var_name,
+                     mcv2 = "MCV2",
+                     GDP_per_capita = "GDP per capita",
+                     measles_incidence_rate_per_1000000_total_population = "Measles")
 
   map_data |>
     leaflet() |>
@@ -69,7 +56,7 @@ plot_leaflet <- function(var, low, high, format, normalize = FALSE) {
                 color = "white",
                 weight = 1,
                 opacity = 1,
-                label = ~paste0(name, ": ",
+                label = ~paste0(name, " ", var_label, ": ",
                                 ifelse(is.na(scale_col), "No Data", label_col)))
 }
 
@@ -90,4 +77,52 @@ validate_var <- function(var_name) {
              GDP_per_capita,
              measles_incidence_rate_per_1000000_total_population")
   }
+}
+
+#' Color Checker
+#'
+#' @param color The color input to be checked
+#'
+#' @importFrom grDevices col2rgb
+#'
+#' @examples
+#' mmR:::validate_color("#ecf0f1")
+validate_color <- function(color) {
+  if (!is.character(color)){
+    stop("both low and high should be strings")
+  }
+
+  tryCatch({
+      col2rgb(color)
+      TRUE
+    },
+    error = function(e) {
+      stop("check that both low and high are actual colors")
+    })
+}
+
+#' Creating Formatted Data
+#'
+#' @param var_name The name of the variable chosen
+#' @param format Whether or not to format the variable (i.e. percents, dollar
+#' amounts, commas)
+#'
+#' @import dplyr
+#'
+#' @examples
+#' mmR:::formatted_data("mcv2", TRUE)
+formatted_data <- function(var_name, format) {
+  load_data() |>
+      filter(year == 2024) |>
+      mutate(label_col = if (format) {
+        switch(var_name,
+               "mcv2" = percent(.data[[var_name]]),
+               "GDP_per_capita" = dollar(.data[[var_name]]),
+               "measles_incidence_rate_per_1000000_total_population" = comma(.data[[var_name]]))
+        }
+                         else .data[[var_name]],
+             scale_col = if (var_name != "mcv2") {
+               rescale(.data[[var_name]], to = c(0, 1))
+               }
+                         else .data[[var_name]])
 }
--- tests/testthat/test-plot-leaflet.R
+++ tests/testthat/test-plot-leaflet.R
@@ -1,3 +1,42 @@
+test_that("leaflet plot works", {
+  leaflet_plot_mcv2 <- plot_leaflet(var = mcv2,
+                                    low = "#ecf0f1",
+                                    high = "#e74c3c")
+  leaflet_plot_gdp <- plot_leaflet(var = GDP_per_capita,
+                                   low = "#ecf0f1",
+                                   high = "#1ccdaa",
+                                   format = TRUE)
+  leaflet_plot_measles1 <- plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
+                                        low = "#ecf0f1",
+                                        high = "#f4a324",
+                                        format = TRUE)
+  leaflet_plot_measles2 <- plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
+                                        low = "#ecf0f1",
+                                        high = "#f4a324",
+                                        format = FALSE)
+
+  expect_s3_class(leaflet_plot_mcv2, "leaflet")
+  expect_s3_class(leaflet_plot_gdp, "leaflet")
+  expect_s3_class(leaflet_plot_measles1, "leaflet")
+  expect_s3_class(leaflet_plot_measles2, "leaflet")
+})
+
+test_that("leaflet plot checks inputs", {
+  expect_error(plot_leaflet(var = something,
+                            low = 1,
+                            high = 2))
+  expect_error(plot_leaflet(var = mcv2,
+                            low = 1,
+                            high = 2))
+  expect_error(plot_leaflet(var = GDP_per_capita,
+                            low = "something",
+                            high = "something else"))
+  expect_error(plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
+                            low = "#ecf0f1",
+                            high = "#f4a324",
+                            format = "FALSE"))
+})
+
 test_that("var validation works", {
   expect_silent(validate_var("mcv2"))
   expect_silent(validate_var("GDP_per_capita"))
@@ -6,40 +45,28 @@ test_that("var validation works", {
   expect_error(validate_var("sleep_rate"))
 })
 
-test_that("leaflet plot works", {
-  leaflet_plot_mcv2 <- plot_leaflet(var = mcv2,
-                                    low = "#ecf0f1",
-                                    high = "#e74c3c",
-                                    format = "percent")
-  leaflet_plot_gdp <- plot_leaflet(var = GDP_per_capita,
-                                   low = "#ecf0f1",
-                                   high = "#1ccdaa",
-                                   format = "dollar",
-                                   normalize = TRUE)
-  leaflet_plot_measles <- plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
-                                       low = "#ecf0f1",
-                                       high = "#f4a324",
-                                       format = "comma",
-                                       normalize = TRUE)
+test_that("color validation works", {
+  expect_true(validate_color("#ecf0f1"))
+  expect_true(validate_color("#e74c3c"))
+  expect_true(validate_color("forestgreen"))
 
-  expect_s3_class(leaflet_plot_mcv2, "leaflet")
-  expect_s3_class(leaflet_plot_gdp, "leaflet")
-  expect_s3_class(leaflet_plot_measles, "leaflet")
+  expect_error(validate_color(1))
+  expect_error(validate_color("something"))
 })
 
-test_that("leaflet plot checks inputs", {
-  expect_error(plot_leaflet(var = mcv2,
-                            low = 1,
-                            high = 2,
-                            format = "percent"))
-  expect_error(plot_leaflet(var = GDP_per_capita,
-                            low = "#ecf0f1",
-                            high = "#1ccdaa",
-                            format = "something else",
-                            normalize = TRUE))
-  expect_error(plot_leaflet(var = measles_incidence_rate_per_1000000_total_population,
-                            low = "#ecf0f1",
-                            high = "#f4a324",
-                            format = "comma",
-                            normalize = "TRUE"))
+test_that("formatting data works", {
+  mcv2_result <- formatted_data("mcv2", TRUE)
+  expect_s3_class(mcv2_result, "data.frame")
+  expect_equal(mcv2_result$mcv2, mcv2_result$scale_col)
+
+  gdp_results <- formatted_data("GDP_per_capita", TRUE)
+  expect_s3_class(gdp_results, "data.frame")
+  expect_gte(min(gdp_results$scale_col, na.rm = TRUE), 0)
+  expect_lte(max(gdp_results$scale_col, na.rm = TRUE), 1)
+
+  measles_results <- formatted_data("measles_incidence_rate_per_1000000_total_population",
+                                    FALSE)
+  expect_s3_class(measles_results, "data.frame")
+  expect_equal(measles_results$measles_incidence_rate_per_1000000_total_population,
+               measles_results$label_col)
 })
